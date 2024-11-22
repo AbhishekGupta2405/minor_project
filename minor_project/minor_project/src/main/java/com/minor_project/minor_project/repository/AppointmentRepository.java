25ea5a4005db528437ecff46bd35b32858eb7c09
Amansjcnsj <ssaman7566@gmail.com>
2024-11-22 12:46:38 +0530

    Delete minor_project/minor_project directory

--- minor_project/minor_project/src/main/java/com/minor_project/minor_project/repository/AppointmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.minor_project.minor_project.repository;
-
-
-import com.minor_project.minor_project.Entity.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- minor_project/minor_project/src/main/java/com/minor_project/minor_project/repository/WorkerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.minor_project.minor_project.repository;
-
-import com.minor_project.minor_project.Entity.Worker;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
